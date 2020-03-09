a983cfc4ab51f120df3c010c17ecb87baeb16dc0
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2020-03-09 12:46:14 +0100

    Update pydval.py

--- pydval.py
+++ pydval.py
@@ -4,13 +4,15 @@
 # Data Validator By R&D ICWR      #
 # Copyright (c)2020 - Afrizal F.A #
 ###################################
+
 import re
 
 class validator():
-    def __init__(self):
-        pass
-    
-    def matching(self,key,data):
+    def __init__(self,key,data):
+        self.key=key
+        self.data=data
+
+    def proc(self,key,data):
         valid_data=[]
         for x in key:
             array_data=re.findall(x.lower(),data.lower())
@@ -18,36 +20,13 @@ class validator():
                 valid_data.append(array_data)
         return valid_data
 
-    def multi_matching(self,data_key,data_input):
-        valid_data=[]
-        i=0
-        for x in data_input:
-            i=i+1
-            valid_data.append({i:self.matching(data_key,x)})
-        return valid_data
-
-###################################
-# Data Validator By R&D ICWR      #
-# Copyright (c)2020 - Afrizal F.A #
-###################################
-import re
-
-class validator():
-    def __init__(self):
-        pass
-    
-    def matching(self,key,data):
-        valid_data=[]
-        for x in key:
-            array_data=re.findall(x.lower(),data.lower())
-            if array_data:
-                valid_data.append(array_data)
-        return valid_data
-
-    def multi_matching(self,data_key,data_input):
-        valid_data=[]
-        i=0
-        for x in data_input:
-            i=i+1
-            valid_data.append({i:self.matching(data_key,x)})
-        return valid_data
+    def matching(self):
+        if type(self.data) == list:
+            valid_data=[]
+            i=0
+            for x in self.data:
+                i=i+1
+                valid_data.append({i:self.proc(self.key,x)})
+            return valid_data
+        else:
+            return self.proc(self.key,self.data)
