5986cea6be0b6a939634c8b4630729b0714b4442
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2020-03-09 10:52:04 +0700

    Create pydv.py

---
+++ pydv.py
@@ -0,0 +1,25 @@
+###################################
+# Data Validator By R&D ICWR      #
+# Copyright (c)2020 - Afrizal F.A #
+###################################
+import re
+
+class validator():
+    def __init__(self):
+        pass
+    
+    def matching(self,key,data):
+        valid_data=[]
+        for x in key:
+            array_data=re.findall(x.lower(),data.lower())
+            if array_data:
+                valid_data.append(array_data)
+        return valid_data
+
+    def multi_matching(self,data_key,data_input):
+        valid_data=[]
+        i=0
+        for x in data_input:
+            i=i+1
+            valid_data.append({i:self.matching(data_key,x)})
+        return valid_data
